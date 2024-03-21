735c97aa7a8d1bbf7bbeaf0d9d8e4b510296dfde
ShehabElDin <SE2101590@tkh.edu.eg>
2024-03-21 23:48:56 +0200

    added hashing function, and double hashing for resolving collisions

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 // Declaring a variable to store the length of the hash table
-const int array_num = 100;
+const int ARRAY_SIZE = 100;
 
 // Constructor for the password manager
 struct PasswordEntry {
     string username;
     string encryptedPassword;
 
-
+    PasswordEntry() : username(""), encryptedPassword("") {}
     PasswordEntry(const string& user, const string& encPass) : username(user), encryptedPassword(encPass) {}
 };
 
@@ -34,4 +34,23 @@ string decrypt(const string& encryptedPassword) {
         decrypted += c ^ XOR_KEY;
     }
     return decrypted;
+}
+
+// hash_fn is used to calculate the hash value for the hash tables
+
+int hash_fn(const string& key) {
+    int hash = 0;
+    for (char c : key) {
+        hash += c;
+    }
+    return hash % ARRAY_SIZE;
+}
+
+// (Double hashing) Utilizing a second hash value in order to resolve collisions
+int secondary_hash_fn(const string& key) {
+    int hash = 0;
+    for (char c : key) {
+        hash += c * 31; // Using a different prime multiplier for diversity
+    }
+    return (hash % (ARRAY_SIZE - 1)) + 1; // Ensure non-zero and less than ARRAY_SIZE
 }
