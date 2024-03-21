76133210621d6869c85103dd1f58e467f8b3f335
ShehabElDin <SE2101590@tkh.edu.eg>
2024-03-21 17:15:21 +0200

    added encryption function

--- main.cpp
+++ main.cpp
@@ -14,3 +14,13 @@ struct PasswordEntry {
 
     PasswordEntry(const string& user, const string& encPass) : username(user), encryptedPassword(encPass) {}
 };
+
+// Encrypting the passwords entered by the user
+string encrypt(const string& password) {
+    const char XOR_KEY = 0x77;
+    string encrypted;
+    for (char c : password) {
+        encrypted += c ^ XOR_KEY;
+    }
+    return encrypted;
+}
