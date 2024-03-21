760d0ccac895fa86b7d8637d44e5bab2aebfde6b
ShehabElDin <SE2101590@tkh.edu.eg>
2024-03-21 17:05:19 +0200

    added constructer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
-// test
+using namespace std;
+
+// Declaring a variable to store the length of the hash table
+const int array_num = 100;
+
+// Constructor for the password manager
+struct PasswordEntry {
+    string username;
+    string encryptedPassword;
+
+
+    PasswordEntry(const string& user, const string& encPass) : username(user), encryptedPassword(encPass) {}
+};
